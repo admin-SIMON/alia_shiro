f1b44ff056cd37997bf8151738db44f9845f03b0
admin-simon <admin_simon@icloud.com>
2019-12-02 17:13:16 +0800

    update

---
+++ src/main/java/com/alia/shiro/controller/LogoutController.java
@@ -0,0 +1,14 @@
+package com.alia.shiro.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * logout, 映射shiro自带过滤器(LogoutFilter)
+ * 写退出的接口也没效果, 被过滤器拦截处理, 并重定向到需要登录的接口
+ *
+ * @author Simon
+ */
+@RestController
+public class LogoutController {
+
+}
